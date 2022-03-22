788d4f9bae84def58ea91302da6f18a3e1527849
Neil Shapiro <nshapiro@slack-corp.com>
2022-03-22 16:33:23 -0700

    Tweak manifest file

--- manifest.ts
+++ manifest.ts
@@ -1,44 +1,40 @@
-import { DefineFunction, Manifest } from "deno-slack-sdk/mod.ts";
+import { DefineFunction, Manifest, Schema } from "deno-slack-sdk/mod.ts";
 
 const ReverseFunction = DefineFunction({
-  "callback_id": "reverse",
-  "title": "Reverse",
-  "description": "Takes a string and reverses it",
-  "source_file": "functions/reverse.ts",
-  "input_parameters": {
-    "required": [
-      "stringToReverse",
-    ],
-    "properties": {
-      "stringToReverse": {
-        "type": "string",
-        "description": "The string to reverse",
+  callback_id: "reverse",
+  title: "Reverse",
+  description: "Takes a string and reverses it",
+  source_file: "functions/reverse.ts",
+  input_parameters: {
+    required: ["stringToReverse"],
+    properties: {
+      stringToReverse: {
+        type: Schema.types.string,
+        description: "The string to reverse",
       },
     },
   },
-  "output_parameters": {
-    "required": [
-      "reverseString",
-    ],
-    "properties": {
-      "reverseString": {
-        "type": "string",
-        "description": "The string in reverse",
+  output_parameters: {
+    required: ["reverseString"],
+    properties: {
+      reverseString: {
+        type: Schema.types.string,
+        description: "The string in reverse",
       },
     },
   },
 });
 
 export default Manifest({
-  "name": "reverse",
-  "description": "Reverse a string",
+  name: "reverse",
+  description: "Reverse a string",
   // Once Manifest APIs support this, we'll add it
   // "runtime_environment": "slack",
-  "runtime": "deno1.x",
+  runtime: "deno1.x",
   // The CLI should still read this and update the icon, then remove if from what's sent to manifest APIs
   // We could have the deno-slack-builder make sure this file is included in the `output` path if helpful
-  "icon": "assets/icon.png",
-  "functions": [ReverseFunction],
-  "outgoingDomains": [],
-  "botScopes": ["commands", "chat:write", "chat:write.public"],
+  icon: "assets/icon.png",
+  functions: [ReverseFunction],
+  outgoingDomains: [],
+  botScopes: ["commands", "chat:write", "chat:write.public"],
 });
