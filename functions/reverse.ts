501bc3b65e832ee42c46e6c86aa9724f1f393211
Neil Shapiro <nshapiro@slack-corp.com>
2022-04-13 11:39:26 -0500

    Use next version of the SDK (#21)
    * Adding the FunctionHandler type to our example function
    
    * Remove unnecessary manifest bits
    
    * Bump version of the SDK

--- functions/reverse.ts
+++ functions/reverse.ts
@@ -1,4 +1,7 @@
-const reverse = async ({ inputs, env }: any) => {
+import type { FunctionHandler } from "deno-slack-sdk/types.ts";
+
+// deno-lint-ignore no-explicit-any
+const reverse: FunctionHandler<any, any> = async ({ inputs, env }) => {
   console.log(`reversing ${inputs.stringToReverse}.`);
   console.log(`SLACK_API_URL=${env["SLACK_API_URL"]}`);
 
