77199f6e920083f441021e86abc7f66a104eecf7
Neil Shapiro <nshapiro@slack-corp.com>
2022-04-14 14:28:30 -0700

    Move the required field

--- manifest.ts
+++ manifest.ts
@@ -6,22 +6,22 @@ const ReverseFunction = DefineFunction({
   description: "Takes a string and reverses it",
   source_file: "functions/reverse.ts",
   input_parameters: {
-    required: ["stringToReverse"],
     properties: {
       stringToReverse: {
         type: Schema.types.string,
         description: "The string to reverse",
       },
     },
+    required: ["stringToReverse"],
   },
   output_parameters: {
-    required: ["reverseString"],
     properties: {
       reverseString: {
         type: Schema.types.string,
         description: "The string in reverse",
       },
     },
+    required: ["reverseString"],
   },
 });
 
