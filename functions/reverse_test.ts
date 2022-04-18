16a07ad6928cf9e064538e129f4b25320a348132
Neil Shapiro <nshapiro@slack-corp.com>
2022-04-18 14:41:13 -0500

    Reignite Release (#26)
    Bump SDK and Hooks versions. Update test to use SlackFunctionTester

--- functions/reverse_test.ts
+++ functions/reverse_test.ts
@@ -1,8 +1,11 @@
+import { SlackFunctionTester } from "deno-slack-sdk/mod.ts";
 import { assertEquals } from "https://deno.land/std@0.99.0/testing/asserts.ts";
 import ReverseString from "./reverse.ts";
 
+const { createContext } = SlackFunctionTester("reverse");
+
 Deno.test("Reverse string function test", async () => {
   const inputs = { stringToReverse: "foo" };
-  const { outputs } = await ReverseString({ inputs, env: {} });
+  const { outputs } = await ReverseString(createContext({ inputs }));
   assertEquals(outputs?.reverseString, "oof");
 });
