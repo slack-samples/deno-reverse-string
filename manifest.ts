18c5cd0d237f1b11e42d6e216eba53e4e1829ef9
Steve Gill <stevengill97@gmail.com>
2022-06-21 21:22:54 -0700

    Added multi step workflow to example

--- manifest.ts
+++ manifest.ts
@@ -1,4 +1,9 @@
-import { DefineFunction, Manifest, Schema } from "deno-slack-sdk/mod.ts";
+import {
+  DefineFunction,
+  DefineWorkflow,
+  Manifest,
+  Schema,
+} from "deno-slack-sdk/mod.ts";
 
 export const ReverseFunction = DefineFunction({
   callback_id: "reverse",
@@ -25,11 +30,38 @@ export const ReverseFunction = DefineFunction({
   },
 });
 
+export const TestReverseWorkflow = DefineWorkflow({
+  callback_id: "test_reverse",
+  title: "Test Reverse Function",
+  description: "test the reverse function",
+  input_parameters: {
+    properties: {
+      "stringToReverse": {
+        type: Schema.types.string,
+      },
+      "channel": {
+        type: Schema.slack.types.channel_id,
+      },
+    },
+    required: ["stringToReverse", "channel"],
+  },
+});
+
+const reverseStep = TestReverseWorkflow.addStep(ReverseFunction, {
+  stringToReverse: TestReverseWorkflow.inputs.stringToReverse,
+});
+
+TestReverseWorkflow.addStep("slack#/functions/send_message", {
+  channel_id: TestReverseWorkflow.inputs.channel,
+  message: reverseStep.outputs.reverseString,
+});
+
 export default Manifest({
   name: "reverse",
   description: "Reverse a string",
   icon: "assets/icon.png",
   functions: [ReverseFunction],
+  workflows: [TestReverseWorkflow],
   outgoingDomains: [],
   botScopes: ["commands", "chat:write", "chat:write.public"],
 });
