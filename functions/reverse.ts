4d992c44c8b28351102a37515f3a13941c49d66d
Brad Harris <bharris@slack-corp.com>
2022-08-30 14:03:10 -0600

    Updating sdk and hooks versions (#46)
    * updating to 1.0.0 of deno-slack-sdk
    
    * updating to latest deno-slack-sdk and deno-slack-hooks versions

--- functions/reverse.ts
+++ functions/reverse.ts
@@ -1,16 +1,12 @@
-import type { SlackFunctionHandler } from "deno-slack-sdk/types.ts";
-import type { ReverseFunction } from "../manifest.ts";
+import { SlackFunction } from "deno-slack-sdk/mod.ts";
+import { ReverseFunction } from "../manifest.ts";
 
-const reverse: SlackFunctionHandler<typeof ReverseFunction.definition> = async (
-  { inputs, env },
-) => {
+export default SlackFunction(ReverseFunction, ({ inputs, env }) => {
   console.log(`reversing ${inputs.stringToReverse}.`);
   console.log(`SLACK_API_URL=${env["SLACK_API_URL"]}`);
 
   const reverseString = inputs.stringToReverse.split("").reverse().join("");
-  return await {
+  return {
     outputs: { reverseString },
   };
-};
-
-export default reverse;
+});
