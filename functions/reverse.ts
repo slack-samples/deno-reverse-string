50737aee53c02a88cc4c227b29d76c16cda73b6e
Alissa Renz <alissa.renz@gmail.com>
2023-04-24 10:06:24 -0700

    Update app and README (GA) (#67)

--- functions/reverse.ts
+++ functions/reverse.ts
@@ -1,12 +1,36 @@
-import { SlackFunction } from "deno-slack-sdk/mod.ts";
-import { ReverseFunction } from "../manifest.ts";
+import { DefineFunction, Schema, SlackFunction } from "deno-slack-sdk/mod.ts";
 
-export default SlackFunction(ReverseFunction, ({ inputs, env }) => {
-  console.log(`reversing ${inputs.stringToReverse}.`);
-  console.log(`SLACK_API_URL=${env["SLACK_API_URL"]}`);
-
-  const reverseString = inputs.stringToReverse.split("").reverse().join("");
-  return {
-    outputs: { reverseString },
-  };
+export const ReverseFunctionDefinition = DefineFunction({
+  callback_id: "reverse",
+  title: "Reverse",
+  description: "Takes a string and reverses it",
+  source_file: "functions/reverse.ts",
+  input_parameters: {
+    properties: {
+      stringToReverse: {
+        type: Schema.types.string,
+        description: "The string to reverse",
+      },
+    },
+    required: ["stringToReverse"],
+  },
+  output_parameters: {
+    properties: {
+      reverseString: {
+        type: Schema.types.string,
+        description: "The string in reverse",
+      },
+    },
+    required: ["reverseString"],
+  },
 });
+
+export default SlackFunction(
+  ReverseFunctionDefinition,
+  ({ inputs }) => {
+    const reverseString = inputs.stringToReverse.split("").reverse().join("");
+    return {
+      outputs: { reverseString },
+    };
+  },
+);
--- manifest.ts
+++ manifest.ts
@@ -1,89 +1,11 @@
-import {
-  DefineFunction,
-  DefineWorkflow,
-  Manifest,
-  Schema,
-} from "deno-slack-sdk/mod.ts";
-
-export const ReverseFunction = DefineFunction({
-  callback_id: "reverse",
-  title: "Reverse",
-  description: "Takes a string and reverses it",
-  source_file: "functions/reverse.ts",
-  input_parameters: {
-    properties: {
-      stringToReverse: {
-        type: Schema.types.string,
-        description: "The string to reverse",
-      },
-    },
-    required: ["stringToReverse"],
-  },
-  output_parameters: {
-    properties: {
-      reverseString: {
-        type: Schema.types.string,
-        description: "The string in reverse",
-      },
-    },
-    required: ["reverseString"],
-  },
-});
-
-export const TestReverseWorkflow = DefineWorkflow({
-  callback_id: "test_reverse",
-  title: "Test Reverse Function",
-  description: "test the reverse function",
-  input_parameters: {
-    properties: {
-      interactivity: {
-        type: Schema.slack.types.interactivity,
-      },
-      channel: {
-        type: Schema.slack.types.channel_id,
-      },
-    },
-    required: ["interactivity"],
-  },
-});
-
-const formData = TestReverseWorkflow.addStep(Schema.slack.functions.OpenForm, {
-  title: "Reverse string form",
-  submit_label: "Submit form",
-  description: "Submit a string to reverse",
-  interactivity: TestReverseWorkflow.inputs.interactivity,
-  fields: {
-    required: ["channel", "stringInput"],
-    elements: [
-      {
-        name: "stringInput",
-        title: "String input",
-        type: Schema.types.string,
-      },
-      {
-        name: "channel",
-        title: "Post in",
-        type: Schema.slack.types.channel_id,
-        default: TestReverseWorkflow.inputs.channel,
-      },
-    ],
-  },
-});
-
-const reverseStep = TestReverseWorkflow.addStep(ReverseFunction, {
-  stringToReverse: formData.outputs.fields.stringInput,
-});
-
-TestReverseWorkflow.addStep(Schema.slack.functions.SendMessage, {
-  channel_id: formData.outputs.fields.channel,
-  message: reverseStep.outputs.reverseString,
-});
+import { Manifest } from "deno-slack-sdk/mod.ts";
+import ReverseWorkflow from "./workflows/reverse_string.ts";
 
 export default Manifest({
-  name: "reverse",
-  description: "Reverse a string",
+  name: "deno-reverse-string",
+  description: "Post the reversed version of a string to a selected channel",
   icon: "assets/default_new_app_icon.png",
-  workflows: [TestReverseWorkflow],
+  workflows: [ReverseWorkflow],
   outgoingDomains: [],
   botScopes: ["commands", "chat:write", "chat:write.public"],
 });
